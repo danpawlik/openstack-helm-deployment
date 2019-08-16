a9609bf1b9b65f63b17edfa78eb04b9164c59b9a
Marek Chmiel <marek.chmiel@corp.ovh.com>
2019-08-16 06:54:39 +0000

    k8s custom images
    Add flexibility of docker repo for k8s_custom_images
    
    (cherry picked from commit 519cc181bc8268396bcfabfbcaaa3592260361e1)
    (cherry picked from commit 3a1eefd46a63ca6bde905cd40fe391ce76127448)

--- scripts/setup-controller.sh
+++ scripts/setup-controller.sh
@@ -59,19 +59,19 @@ fi
 # setup docker.io if bionic; they have hardcoded docker.io from xenial
 if [ "${RELEASE}" = "bionic" ]; then
     sudo tee  /etc/apt/sources.list <<EOF
-deb http://nova.clouds.archive.ubuntu.com/ubuntu/ bionic main restricted universe multiverse
-deb http://nova.clouds.archive.ubuntu.com/ubuntu/ bionic-security main restricted universe multiverse
-deb http://nova.clouds.archive.ubuntu.com/ubuntu/ bionic-updates main restricted universe multiverse
-deb http://nova.clouds.archive.ubuntu.com/ubuntu/ bionic-proposed main restricted universe multiverse
-deb http://nova.clouds.archive.ubuntu.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb http://ubuntu.mirrors.ovh.net/ubuntu/ bionic main restricted universe multiverse
+deb http://ubuntu.mirrors.ovh.net/ubuntu/ bionic-security main restricted universe multiverse
+deb http://ubuntu.mirrors.ovh.net/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://ubuntu.mirrors.ovh.net/ubuntu/ bionic-proposed main restricted universe multiverse
+deb http://ubuntu.mirrors.ovh.net/ubuntu/ bionic-backports main restricted universe multiverse
 EOF
 elif [ "${RELEASE}" = "xenial" ]; then
     sudo tee  /etc/apt/sources.list <<EOF
-deb http://nova.clouds.archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse
-deb http://nova.clouds.archive.ubuntu.com/ubuntu/ xenial-security main restricted universe multiverse
-deb http://nova.clouds.archive.ubuntu.com/ubuntu/ xenial-updates main restricted universe multiverse
-deb http://nova.clouds.archive.ubuntu.com/ubuntu/ xenial-proposed main restricted universe multiverse
-deb http://nova.clouds.archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse
+deb http://ubuntu.mirrors.ovh.net/ubuntu/ xenial main restricted universe multiverse
+deb http://ubuntu.mirrors.ovh.net/ubuntu/ xenial-security main restricted universe multiverse
+deb http://ubuntu.mirrors.ovh.net/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://ubuntu.mirrors.ovh.net/ubuntu/ xenial-proposed main restricted universe multiverse
+deb http://ubuntu.mirrors.ovh.net/ubuntu/ xenial-backports main restricted universe multiverse
 EOF
 fi
 
